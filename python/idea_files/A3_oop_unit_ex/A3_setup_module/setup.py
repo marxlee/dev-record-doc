3f7101704d9731aa961c4d56b8be06790c1eb3dc
Marx <mailmarx@163.com>
2019-08-14 11:48:29 +0800

    Update setup.py

--- python/idea_files/A3_oop_unit_ex/A3_setup_module/setup.py
+++ python/idea_files/A3_oop_unit_ex/A3_setup_module/setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(name="hm_message",  # 包名
       version="1.0",  # 版本
-      description="itheima's 发送和接收消息模块",  # 描述信息
+      description="marx's 发送和接收消息模块",  # 描述信息
       long_description="完整的发送和接收消息模块",  # 完整描述信息
       author="itheima",  # 作者
-      author_email="itheima@itheima.com",  # 作者邮箱
-      url="www.itheima.com",  # 主页
+      author_email="mailmarx@163.com",  # 作者邮箱
+      url="https://github.com/marxlee",  # 主页
       py_modules=["hm_message.send_message",
                   "hm_message.receive_message"])
